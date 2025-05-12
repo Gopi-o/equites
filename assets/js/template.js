f25e0b0ca86313937f03f77f1d5cb18cde063fb2
aqwer723g <leragus006@gmail.com>
2025-05-12 23:09:40 +0400

    добавила хедер и футер, js в отдельные файлы

---
+++ assets/js/template.js
@@ -0,0 +1,9 @@
+ // Скрипт для изменения фона хедера при скролле
+        window.addEventListener('scroll', function() {
+            const header = document.querySelector('header');
+            if (window.scrollY > 50) {
+                header.classList.add('scrolled');
+            } else {
+                header.classList.remove('scrolled');
+            }
+        });
