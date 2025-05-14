4143c2dbeb933b290808cdfe81645cee99d238ae
aqwer723g <leragus006@gmail.com>
2025-05-14 23:41:48 +0400

    удалила

--- assets/js/modules/menu.js
+++
@@ -1,8 +0,0 @@
-        // Улучшенный скрипт для бургер-меню
-            const burgerMenu = document.getElementById('burgerMenu');
-            const mobileMenu = document.getElementById('mobileMenu');
-            
-            burgerMenu.addEventListener('click', function() {
-                this.classList.toggle('active');
-                mobileMenu.classList.toggle('active');
-            });
